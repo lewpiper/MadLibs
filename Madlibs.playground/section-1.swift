719715624b4e4f8795064399771eaef7faf905df
Lew <lewpiper@mac.com>
2014-08-16 10:11:49 -0700

    This is my first commit
    This is my first commit for a Madlibs string document in Swift based
    off a Ray Wenderlich tutorial on Swift. s

---
+++ Madlibs.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//Set string values
+let emotion = ":)"
+let name = "Ray"
+let noun = "swift"
+
+
+//This is called string interpolation where you can inject your variables into your string messages similar to num2str in MatLab
+let madLib = "When \(name) saw \(noun), he was like \(emotion)"
